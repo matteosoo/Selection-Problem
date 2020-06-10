f34a785ccb38ec3977059d3f47904ae4a7bd860d
Matteo Soo <matteosoo11@gmail.com>
2020-06-10 17:42:49 +0800

    debug

--- prune_and_search.cpp
+++ prune_and_search.cpp
@@ -4,22 +4,26 @@
 #include<algorithm> 
 #include<climits> 
 
-using namespace std; 
+#include <vector>
+#include <fstream>
+#include <iomanip>
+using namespace std::chrono;
+using namespace std;
 
-int partition(int arr[], int l, int r, int k); 
+int partition(vector<int>& arr, int l, int r, int k);
 
 // A simple function to find median of arr[]. This is called 
 // only for an array of size 5 in this program. 
-int findMedian(int arr[], int n) 
+int findMedian(vector<int>& arr, int n) 
 { 
-	sort(arr, arr+n); // Sort the array 
+	sort(arr.begin(), arr.begin()+n); // Sort the array 
 	return arr[n/2]; // Return middle element 
 } 
 
 // Returns k'th smallest element in arr[l..r] in worst case 
 // linear time. ASSUMPTION: ALL ELEMENTS IN ARR[] ARE DISTINCT 
-int kthSmallest(int arr[], int l, int r, int k) 
-{ 
+int kthSmallest(vector<int>& arr, int l, int r, int k) 
+{
 	// If k is smaller than number of elements in array 
 	if (k > 0 && k <= r - l + 1) 
 	{ 
@@ -27,24 +31,32 @@ int kthSmallest(int arr[], int l, int r, int k)
 
 		// Divide arr[] in groups of size 5, calculate median 
 		// of every group and store it in median[] array. 
-		int i, median[(n+4)/5]; // There will be floor((n+4)/5) groups; 
+		int i;
+		vector<int> median((n+4)/5); // There will be floor((n+4)/5) groups; 
+		vector<int>::const_iterator first = arr.begin() + l+i*5;
+
 		for (i=0; i<n/5; i++) 
-			median[i] = findMedian(arr+l+i*5, 5); 
+		{
+			vector<int> sub_arr(first, first+5);
+			median[i] = findMedian(sub_arr, 5); 
+		}
 		if (i*5 < n) //For last group with less than 5 elements 
 		{ 
-			median[i] = findMedian(arr+l+i*5, n%5); 
+			vector<int> sub_arr(first, first+(n%5));
+			median[i] = findMedian(sub_arr, n%5); 
 			i++; 
-		}	 
+		}
 
 		// Find median of all medians using recursive call. 
 		// If median[] has only one element, then no need 
 		// of recursive call 
 		int medOfMed = (i == 1)? median[i-1]: 
 								kthSmallest(median, 0, i-1, i/2); 
-
+		
 		// Partition the array around a random element and 
 		// get position of pivot element in sorted array 
-		int pos = partition(arr, l, r, medOfMed); 
+
+		int pos = partition(arr, l, r, medOfMed);
 
 		// If position is same as k 
 		if (pos-l == k-1) 
@@ -60,23 +72,23 @@ int kthSmallest(int arr[], int l, int r, int k)
 	return INT_MAX; 
 } 
 
-void swap(int *a, int *b) 
+void swap(int &a, int &b) 
 { 
-	int temp = *a; 
-	*a = *b; 
-	*b = temp; 
+	int temp = a; 
+	a = b; 
+	b = temp; 
 } 
 
 // It searches for x in arr[l..r], and partitions the array 
 // around x. 
-int partition(int arr[], int l, int r, int x) 
+int partition(vector<int>& arr, int l, int r, int x) 
 { 
 	// Search for x in arr[l..r] and move it to end 
 	int i; 
 	for (i=l; i<r; i++) 
 		if (arr[i] == x) 
 		break; 
-	swap(&arr[i], &arr[r]); 
+	swap(arr[i], arr[r]); 
 
 	// Standard partition algorithm 
 	i = l; 
@@ -84,21 +96,44 @@ int partition(int arr[], int l, int r, int x)
 	{ 
 		if (arr[j] <= x) 
 		{ 
-			swap(&arr[i], &arr[j]); 
+			swap(arr[i], arr[j]); 
 			i++; 
 		} 
 	} 
-	swap(&arr[i], &arr[r]); 
+	swap(arr[i], arr[r]); 
 	return i; 
 } 
 
 // Driver program to test above methods 
-int main() 
+int main(int argc, char* argv[]) 
 { 
-	int arr[] = {12, 3, 5, 7, 4, 19, 26}; 
-	int n = sizeof(arr)/sizeof(arr[0]), k = 3; 
-	cout << "K'th smallest element is "
-		<< kthSmallest(arr, 0, n-1, k); 
+	freopen(argv[1], "r", stdin);
+	int n, k;
+	std::cin >> n >> k;
+	std::cout << n << " " << k << endl;
+
+	vector<int> arr(n); 
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> arr[i];
+	}
+
+	// prune & search method (ps)
+	auto ps_start = high_resolution_clock::now();
+	int ps_result = kthSmallest(arr, 0, n - 1, k);
+	auto ps_stop = high_resolution_clock::now();
+	auto ps_duration = duration_cast<nanoseconds>(ps_stop - ps_start);
+	
+	// write file to PS.txt
+	ofstream ps_output_file("PS.txt");
+	ps_output_file << ps_result << endl;
+	float ps_time = (ps_duration.count())/(1e+9);
+	ps_output_file << std::fixed << std::setprecision(6) << ps_time;
+	ps_output_file.close();
+
+	// screan output
+	std::cout << "K-th smallest element is "	<< ps_result << endl; 
+	std::cout << "Running time of the program " << std::fixed << std::setprecision(6) << ps_time << endl;
 	return 0; 
 } 
 
